80418e9f66846a992bf8fc40bdefaa63acd5b5cc
padowla <62257411+padowla@users.noreply.github.com>
2023-02-17 11:10:11 +0100

    Delete download_title.sh

--- download_title.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-URL_video="$1"
-PATH_DOWNLOAD="/home/pi/MyScript/TelegramBot/Music"
-
-title=`youtube-dl --get-title $URL_video`
-echo -n $title #> "$PATH_DOWNLOAD/.name_song"
-#echo "+++++++++++++++ FINE SCRIPT download_title.sh +++++++++++"
